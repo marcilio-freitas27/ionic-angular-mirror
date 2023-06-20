f7b3e02d2dbb1d025ca7a17d2295a03abaaf9452
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-06-20 00:11:22 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
+
+const cameraPreviewOptions: CameraPreviewOptions = {
+  position: 'front',
+  height: 1920,
+  width: 1080
+};
+CameraPreview.start(cameraPreviewOptions);
 
 @Component({
   selector: 'app-home',
